c307e0e14ace5656ce07347383b2a3e2f5211695
Thiago Nelsi do Couto <thiagonelsi@gmail.com>
2020-05-06 13:28:23 -0300

    Finish the route to get word meaning

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/:word', async (req, res) => {
+
+    const { word } = req.params;
+
+    try {
+        const { data: dicioHTML } = await axios.get(`https://dicio.com.br/${word}`);
+
+        const $ = cheerio.load(dicioHTML);
+
+        const meanings = [];
+
+        $('.significado span').each((_, element) => {
+            const text = $(element).text();
+            meanings.push(text);
+        });
+
+        res.json([...meanings]);
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+
+});
+
+app.listen(3333);
