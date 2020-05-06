375484f50821e1f765fea1132d7d0898e9cb9b57
Thiago Nelsi do Couto <thiagonelsi@gmail.com>
2020-05-06 16:31:45 -0300

    Add editorconfig and change the size of the indentation to 2 spaces

--- src/index.js
+++ src/index.js
@@ -6,37 +6,37 @@ const app = express();
 
 app.get('/:word', async (req, res) => {
 
-    const { word } = req.params;
+  const { word } = req.params;
 
-    try {
+  try {
 
-        const { data: dicioHTML } = await axios.get(`https://dicio.com.br/${word}`);
+    const { data: dicioHTML } = await axios.get(`https://dicio.com.br/${word}`);
 
-        const $ = cheerio.load(dicioHTML);
+    const $ = cheerio.load(dicioHTML);
 
-        const meanings = [];
+    const meanings = [];
 
-        $('.significado span').each((_, element) => {
+    $('.significado span').each((_, element) => {
 
-            const text = $(element).text();
-            const cheerioElement = $(element);
+      const text = $(element).text();
+      const cheerioElement = $(element);
 
-            if (cheerioElement.hasClass('cl'))
-                meanings.push({ class: text, meanings: [], etymology: '' });
+      if (cheerioElement.hasClass('cl'))
+        meanings.push({ class: text, meanings: [], etymology: '' });
 
-            else if (cheerioElement.hasClass('etim'))
-                meanings[meanings.length - 1].etymology = text;
+      else if (cheerioElement.hasClass('etim'))
+        meanings[meanings.length - 1].etymology = text;
 
-            else if (!cheerioElement.hasClass('tag'))
-                meanings[meanings.length - 1].meanings.push(text);
+      else if (!cheerioElement.hasClass('tag'))
+        meanings[meanings.length - 1].meanings.push(text);
 
-        });
+    });
 
-        res.json(meanings);
+    res.json(meanings);
 
-    } catch (err) {
-        res.status(400).json({ error: err.message })
-    }
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
 
 });
 
