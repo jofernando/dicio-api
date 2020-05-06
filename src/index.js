6207c0841735aee7586753d1280b84dd0c61aa84
Thiago Nelsi do Couto <thiagonelsi@gmail.com>
2020-05-06 16:25:32 -0300

    Change the structure of the response

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.get('/:word', async (req, res) => {
     const { word } = req.params;
 
     try {
+
         const { data: dicioHTML } = await axios.get(`https://dicio.com.br/${word}`);
 
         const $ = cheerio.load(dicioHTML);
@@ -16,15 +17,28 @@ app.get('/:word', async (req, res) => {
         const meanings = [];
 
         $('.significado span').each((_, element) => {
+
             const text = $(element).text();
-            meanings.push(text);
+            const cheerioElement = $(element);
+
+            if (cheerioElement.hasClass('cl'))
+                meanings.push({ class: text, meanings: [], etymology: '' });
+
+            else if (cheerioElement.hasClass('etim'))
+                meanings[meanings.length - 1].etymology = text;
+
+            else if (!cheerioElement.hasClass('tag'))
+                meanings[meanings.length - 1].meanings.push(text);
+
         });
 
-        res.json([...meanings]);
+        res.json(meanings);
+
     } catch (err) {
         res.status(400).json({ error: err.message })
     }
 
 });
+
 
 app.listen(process.env.PORT || 3333);
