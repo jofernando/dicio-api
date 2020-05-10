e7595d38945569c044d1ca6b92e5291d16843b7c
Thiago Nelsi do Couto <thiagonelsi@gmail.com>
2020-05-09 22:35:12 -0300

    Fixes #1

--- src/index.js
+++ src/index.js
@@ -15,16 +15,41 @@ app.get('/:word', async (req, res) => {
     const $ = cheerio.load(dicioHTML);
 
     const meanings = [];
+    const structure = {
+      class: '',
+      meanings: [],
+      etymology: ''
+    }
+    meanings.push(structure);
 
     $('.significado span').each((_, element) => {
 
       const text = $(element).text();
       const cheerioElement = $(element);
 
-      if (cheerioElement.hasClass('cl'))
-        meanings.push({ class: text, meanings: [], etymology: '' });
+      if (cheerioElement.hasClass('cl')) {
+        if (meanings.length === 1 && meanings[0].class === '' && meanings[0].meanings.length === 0)
+          meanings[0].class = text;
+        else
+          meanings.push({ class: text, meanings: [], etymology: '' });
+      }
 
       else if (cheerioElement.hasClass('etim'))
+        meanings[meanings.length - 1].etymology = text;
+
+      else if (!cheerioElement.hasClass('tag'))
+        meanings[meanings.length - 1].meanings.push(text);
+
+    });
+
+    if ($('.conjugacao').html()) meanings.push({ ...structure, meanings: [] });
+
+    $('.conjugacao span').each((_, element) => {
+
+      const text = $(element).text();
+      const cheerioElement = $(element);
+
+      if (cheerioElement.hasClass('etim'))
         meanings[meanings.length - 1].etymology = text;
 
       else if (!cheerioElement.hasClass('tag'))
@@ -35,6 +60,7 @@ app.get('/:word', async (req, res) => {
     res.json(meanings);
 
   } catch (err) {
+    console.log(err);
     res.status(400).json({ error: err.message })
   }
 
