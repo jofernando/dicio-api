6bfd0d50756a4478032b36159bc5a01b595ad8b0
Agronault <xande.luis@hotmail.com>
2020-08-29 18:48:34 -0300

    feat(syllables): adds syllables info endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const meanings = require('./meanings');
 const synonyms = require('./synonyms');
+const syllables = require('./syllables');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.get('/:word', meanings);
 
 app.get('/meanings/:word', meanings);
 app.get('/synonyms/:word', synonyms);
+app.get('/syllables/:word', syllables);
 
 
 app.listen(process.env.PORT || 3333);
