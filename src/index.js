032f808102ef916870937dd6b264d33ef9b82879
Agronault <xande.luis@hotmail.com>
2020-08-29 00:12:13 -0300

    feat(synonyms): creates synonym retriever [+]endpoint /meanings/:word [+]endpoint /synonyms/:word obs: old /:word endpoint preserved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const meanings = require('./meanings');
+const synonyms = require('./synonyms');
 
 const app = express();
 
 app.get('/:word', meanings);
+
+app.get('/meanings/:word', meanings);
+app.get('/synonyms/:word', synonyms);
 
 
 app.listen(process.env.PORT || 3333);
